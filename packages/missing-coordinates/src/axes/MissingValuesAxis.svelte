840613f176ad661b1d518ee38cad7021ca95158b
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2022-04-08 14:06:23 +0200

    feat: Introduce `MissingValuesAxis` component

---
+++ packages/missing-coordinates/src/axes/MissingValuesAxis.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { drawConfig, axes } from "../stores";
+
+  $: axisExtremes = getAxisExtremes();
+
+  function getAxisExtremes(): { min: number, max: number } {
+    const offsets = $axes.map(axis => axis.offset);
+    return {
+        min: Math.min(...offsets),
+        max: Math.max(...offsets)
+    }
+  }
+</script>
+
+<line
+  id="missing-values-axis"
+  x1={axisExtremes.min}
+  x2={axisExtremes.max}
+  y1={$drawConfig.axisHeight + $drawConfig.missingValuesAxisSpacing}
+  y2={$drawConfig.axisHeight + $drawConfig.missingValuesAxisSpacing}
+  stroke="black"
+/>
