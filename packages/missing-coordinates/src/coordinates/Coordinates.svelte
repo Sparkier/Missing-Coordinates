969503d1445c5de0fa34591c68ad1104d3c45de2
Alex <alex.baeuerle@uni-ulm.de>
2022-06-02 12:09:27 +0200

    feat: enable coordinate order randomization
    The drawing order of the coordinates can now be randomized.
    
    fix #159

--- packages/missing-coordinates/src/coordinates/Coordinates.svelte
+++ packages/missing-coordinates/src/coordinates/Coordinates.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
   import Coordinate from "./Coordinate.svelte";
-  import { coordinates } from "../stores";
+  import { coordinates, drawConfig } from "../stores";
+  import { DrawingOrder, Coordinate as Coord } from "../types";
+
+  // Get coordinates with appropriate order.
+  $: orderedCoordinates =
+    $drawConfig.drawingOrder === DrawingOrder.RANDOM
+      ? shuffleCoordinates($coordinates)
+      : $coordinates;
+
+  /**
+   * Shuffle coordinate order.
+   *
+   * @param coordinates the coordinates to be shuffled.
+   * @returns shuffled veresion of the coordinates as new array.
+   */
+  function shuffleCoordinates(coordinates: Coord[]): Coord[] {
+    const coords = [...coordinates];
+    for (let index = coords.length - 1; index > 0; index--) {
+      const newIndex = Math.floor(Math.random() * (index + 1));
+      [coords[index], coords[newIndex]] = [coords[newIndex], coords[index]];
+    }
+
+    return coords;
+  }
 </script>
 
 <g>
-  {#each $coordinates as coordinate}
+  {#each orderedCoordinates as coordinate}
     <Coordinate {coordinate} />
   {/each}
 </g>
