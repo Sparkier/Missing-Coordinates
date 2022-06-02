9f1c4a7bc41c841cf557e20ab16757234624816d
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2022-06-02 15:38:41 +0200

    feat: implement `ScaleSequential`

---
+++ packages/missing-coordinates/src/color/scaleSequential.ts
@@ -0,0 +1,53 @@
+import type { HSL } from "./sequentialColorInterpolators";
+
+export default class ScaleSequential {
+  // domain of possible values
+  private domainExtrema: {
+    start: number;
+    end: number;
+  } = { start: 0, end: 0 };
+
+  private interpolateFn: ((t: number) => HSL) | null = null;
+  // range to be projected to
+  // private rangeItems: string[] = [];
+
+  public scale(t: number): string | null {
+    const min = Math.min(this.domainExtrema.start, this.domainExtrema.end);
+    const max = Math.max(this.domainExtrema.start, this.domainExtrema.end);
+    const isWithinDomain = t >= min && t <= max;
+
+    // ensure t is within the domain
+    if (!isWithinDomain) {
+      if (t < min) {
+        t = min;
+      }
+      if (t > max) {
+        t = max;
+      }
+    }
+
+    let normalizedT = (t - min) / (max - min);
+    console.log(normalizedT);
+    if (this.domainExtrema.start > this.domainExtrema.end) {
+      normalizedT = 1 - normalizedT;
+    }
+
+    if (this.interpolateFn !== null) {
+      const hsl = this.interpolateFn(normalizedT);
+      console.log(hsl.h, hsl.s, hsl.l);
+      return this.interpolateFn(normalizedT).hex();
+    }
+    return null;
+  }
+
+  public domain(newDomain: [number, number]): ScaleSequential {
+    const [start, end] = newDomain;
+    this.domainExtrema = { start, end };
+    return this;
+  }
+
+  public interpolator(interpolateFn: (t: number) => HSL): ScaleSequential {
+    this.interpolateFn = interpolateFn;
+    return this;
+  }
+}
