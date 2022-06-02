7e9d0a3d557d9c19016649b9d79e9fc541b60dfc
onc <christian.van-onzenoodt@uni-ulm.de>
2022-06-02 16:06:47 +0200

    feat: implementing oridinal colorscaling (#171)
    * feat: implementing oridinal colorscaling
    
    * Rewrite ScaleOridinal to fix typing issues
    
    * fix: make `schemeCategorical` start with lower letter
    
    * fix: rename `scale` to `valueAt`

---
+++ packages/missing-coordinates/src/color/scaleOrdinal.ts
@@ -0,0 +1,36 @@
+export default class ScaleOrdinal {
+  // mapping from domain values to index.
+  private domainIndex = new Map();
+  // domain of possible values
+  private domainItems: string[] = [];
+  // range to be projected to
+  private rangeItems: string[] = [];
+
+  public valueAt(value: string | number): string | null {
+    const index = this.domainIndex.get(value.toString());
+    if (index === undefined) {
+      return null;
+    }
+    return this.rangeItems[index % this.rangeItems.length];
+  }
+
+  public domain(newDomain: string[] | number[]): ScaleOrdinal {
+    this.domainItems = [];
+    this.domainIndex = new Map();
+
+    newDomain.forEach((value) => {
+      const stringValue = value.toString();
+      if (!this.domainIndex.has(stringValue)) {
+        this.domainIndex.set(stringValue, this.domainItems.length);
+        this.domainItems.push(stringValue);
+      }
+    });
+
+    return this;
+  }
+
+  public range(newRange: string[]): ScaleOrdinal {
+    this.rangeItems = newRange;
+    return this;
+  }
+}
