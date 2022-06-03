45295be262298f49941aa80458b07f51d3afd1a5
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2022-06-03 15:31:06 +0200

    fix: move gradient generation to `Gradients.svelte`

--- packages/missing-coordinates/src/PC.svelte
+++ packages/missing-coordinates/src/PC.svelte
@@ -13,10 +13,9 @@
     drawConfig,
     axisLabelTopOffset,
     axisAnnotationHeight,
-    colorScale,
     data as storeData,
   } from "./stores";
-  import type { Data, AxisData } from "./types";
+  import type { Data } from "./types";
   import { DrawConfiguration, Concept, Variation } from "./types";
 
   export let drawConfiguration: DrawConfiguration = new DrawConfiguration();
@@ -24,38 +23,12 @@
 
   $: drawConfig.set(drawConfiguration);
   $: storeData.set(data);
-  $: usedColors = getUsedColors(data, drawConfiguration, $colorScale);
-
-  function getUsedColors(data: Data, drawConfig: DrawConfiguration, scale: ColorScale | null): string[] {
-    if (scale === null) {
-      return [];
-    }
-
-    const getUniqueValues = (data: AxisData[], colorAxis: string): (string | number)[] => {
-      const axis = data.find((axis) => axis.name === colorAxis);
-      if (!axis) {
-        return [];
-      }
-      return [...new Set(axis.data)].filter((e) => e !== null) as (string | number)[];
-    }
-
-    const coloringAxis = drawConfig.coloring.coloringAxis;
-    const uniqueValues = getUniqueValues(data.axes, coloringAxis)
-    return uniqueValues.map((value: string | number) => {
-      return scale.valueAt(value);
-    }).filter((e) => e !== null) as string[];
-  }
-
 </script>
 
 <main>
   <svg width={$width} height={$height}>
     {#if $drawConfig.variation === Variation.GRADIENT}
-      <defs>
-        {#each usedColors as c}
-          <Gradients color={c} />
-        {/each}
-      </defs>
+      <Gradients />
     {/if}
     <g
       id="main-group"
--- packages/missing-coordinates/src/gradients/Gradients.svelte
+++ packages/missing-coordinates/src/gradients/Gradients.svelte
@@ -1,21 +1,49 @@
 <script lang="ts">
-  export let color: string;
+  import type { Data, DrawConfiguration } from "../types";
+  import type { ColorScale } from "../color/scales";
+  import { drawConfig, data, colorScale } from "../stores";
+
+  $: usedColors = getUsedColors($data, $drawConfig, $colorScale);
+
+  function getUsedColors(data: Data, drawConfig: DrawConfiguration, scale: ColorScale | null): string[] {
+    if (scale === null) {
+      return [];
+    }
+
+    const getUniqueValues = (data: AxisData[], colorAxis: string): (string | number)[] => {
+      const axis = data.find((axis) => axis.name === colorAxis);
+      if (!axis) {
+        return [];
+      }
+      return [...new Set(axis.data)].filter((e) => e !== null) as (string | number)[];
+    }
+
+    const coloringAxis = drawConfig.coloring.coloringAxis;
+    const uniqueValues = getUniqueValues(data.axes, coloringAxis)
+    return uniqueValues.map((value: string | number) => {
+      return scale.valueAt(value);
+    }).filter((e) => e !== null) as string[];
+  }
 </script>
 
-<linearGradient id={`missingGradientDecreasing-${color}`}>
-  <stop offset="0%" stop-opacity="1" stop-color={color} />
-  <stop offset="50%" stop-opacity="0.75" stop-color={color} />
-  <stop offset="100%" stop-opacity="0" stop-color={color} />
-</linearGradient>
-
-<linearGradient id={`missingGradientIncreasing-${color}`}>
-  <stop offset="0%" stop-opacity="0" stop-color={color} />
-  <stop offset="50%" stop-opacity="0.75" stop-color={color} />
-  <stop offset="100%" stop-opacity="1" stop-color={color} />
-</linearGradient>
-
-<linearGradient id={`missingGradientNull-${color}`}>
-  <stop offset="0%" stop-opacity="0" stop-color={color} />
-  <stop offset="50%" stop-opacity="0" stop-color={color} />
-  <stop offset="100%" stop-opacity="0" stop-color={color} />
-</linearGradient>
+<defs>
+  {#each usedColors as color}
+    <linearGradient id={`missingGradientDecreasing-${color}`}>
+      <stop offset="0%" stop-opacity="1" stop-color={color} />
+      <stop offset="50%" stop-opacity="0.75" stop-color={color} />
+      <stop offset="100%" stop-opacity="0" stop-color={color} />
+    </linearGradient>
+    
+    <linearGradient id={`missingGradientIncreasing-${color}`}>
+      <stop offset="0%" stop-opacity="0" stop-color={color} />
+      <stop offset="50%" stop-opacity="0.75" stop-color={color} />
+      <stop offset="100%" stop-opacity="1" stop-color={color} />
+    </linearGradient>
+    
+    <linearGradient id={`missingGradientNull-${color}`}>
+      <stop offset="0%" stop-opacity="0" stop-color={color} />
+      <stop offset="50%" stop-opacity="0" stop-color={color} />
+      <stop offset="100%" stop-opacity="0" stop-color={color} />
+    </linearGradient>
+  {/each}
+</defs>
