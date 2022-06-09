55c75f8602e2fb11f222791260bd4460fb69f0c6
Alex <alex.baeuerle@uni-ulm.de>
2022-06-09 11:24:45 +0200

    refactor: update the defaults for the drawConfiguration

--- packages/storybook/stories/PCDemo.stories.js
+++ packages/storybook/stories/PCDemo.stories.js
@@ -13,9 +13,9 @@ import irisComplete from "./datasets/iris.json";
 import mpgComplete from "./datasets/mpg.json";
 import penguinsComplete from "./datasets/penguins.json";
 
-import { flatten, unflatten } from 'flat';
+import { flatten, unflatten } from "flat";
 
-const delimiter = ' > ';
+const delimiter = " > ";
 
 export default {
   title: "missing-coordinates/PC",
@@ -23,42 +23,45 @@ export default {
   argTypes: {
     "drawConfiguration > concept": {
       options: [0, 1, 2],
-      control: { type: 'radio', labels: ["Default", "Axis", "Imputation"] },
+      control: { type: "radio", labels: ["Default", "Axis", "Imputation"] },
     },
     "drawConfiguration > variation": {
       options: [0, 1, 2, 3, 4],
-      control: { type: 'radio', labels: ["Default", "Opacity", "Gradient", "Dashed", "Glyph"] },
+      control: {
+        type: "radio",
+        labels: ["Default", "Opacity", "Gradient", "Dashed", "Glyph"],
+      },
     },
     "drawConfiguration > coloring > sequentialColorTheme": {
-      options: ['cool', 'warm'],
-      control: { type: 'radio' },
+      options: ["cool", "warm"],
+      control: { type: "radio" },
     },
     "drawConfiguration > coloring > ordinalColorTheme": {
-      options: ['Category10', 'Tableau10'],
-      control: { type: 'radio' },
+      options: ["Category10", "Tableau10"],
+      control: { type: "radio" },
     },
     "drawConfiguration > lineOpacity": {
-      control: { type: 'range', min: 0, max: 1, step: 0.01 },
+      control: { type: "range", min: 0, max: 1, step: 0.01 },
     },
     "drawConfiguration > missingValuesConfiguration > missingValueOpacity": {
-      control: { type: 'range', min: 0, max: 1, step: 0.01 },
+      control: { type: "range", min: 0, max: 1, step: 0.01 },
     },
     "drawConfiguration > missingValuesConfiguration > gradientFalloff": {
-      control: { type: 'range', min: 0, max: 1, step: 0.01 },
+      control: { type: "range", min: 0, max: 1, step: 0.01 },
     },
     "drawConfiguration > axisAnnotationConfiguration > textAnchor": {
-      options: ['start', 'middle', 'end'],
-      control: { type: 'inline-radio' },
+      options: ["start", "middle", "end"],
+      control: { type: "inline-radio" },
     },
     "drawConfiguration > axisLabelConfiguration > textAnchor": {
-      options: ['start', 'middle', 'end'],
-      control: { type: 'inline-radio' },
+      options: ["start", "middle", "end"],
+      control: { type: "inline-radio" },
     },
-    "data": {
+    data: {
       table: {
         disable: true,
       },
-    }
+    },
   },
 };
 
@@ -94,8 +97,8 @@ const PenguinCompleteTemplate = ({ ...args }) => ({
 
 const defaultArgs = {
   drawConfiguration: {
-    concept: Concept.INFORMATION_REMOVAL,
-    variation: Variation.DEFAULT,
+    concept: Concept.IMPUTATION,
+    variation: Variation.DASHED,
     axesSpacing: 120,
     axisHeight: 200,
     coloring: {
@@ -131,90 +134,124 @@ const defaultArgs = {
 
 export const Sample = SampleTemplate.bind({});
 Sample.args = {
-  ...flatten({
-    drawConfiguration: {
-      ...defaultArgs.drawConfiguration,
-      coloring: {
-        ...defaultArgs.drawConfiguration.coloring,
-        coloringAxis: "dim 1",
+  ...flatten(
+    {
+      drawConfiguration: {
+        ...defaultArgs.drawConfiguration,
+        coloring: {
+          ...defaultArgs.drawConfiguration.coloring,
+          coloringAxis: "dim 1",
+        },
       },
     },
-  }, { delimiter }),
+    { delimiter }
+  ),
   data: sample,
 };
 
 export const AirlineComplete = AirlineCompleteTemplate.bind({});
 AirlineComplete.args = {
-  ...flatten({
-    drawConfiguration: {
-      ...defaultArgs.drawConfiguration,
-      axesSpacing: 150,
-      axisHeight: 300,
-      margin: { ...defaultArgs.drawConfiguration.margin, left: 80, right: 80 },
-      coloring: {
-        ...defaultArgs.drawConfiguration.coloring,
-        coloringAxis: "fatalities_00_14",
+  ...flatten(
+    {
+      drawConfiguration: {
+        ...defaultArgs.drawConfiguration,
+        axesSpacing: 150,
+        axisHeight: 300,
+        margin: {
+          ...defaultArgs.drawConfiguration.margin,
+          left: 80,
+          right: 80,
+        },
+        coloring: {
+          ...defaultArgs.drawConfiguration.coloring,
+          coloringAxis: "fatalities_00_14",
+        },
       },
     },
-  }, { delimiter }),
+    { delimiter }
+  ),
   data: airlineSafetyComplete,
 };
 
 export const DriverComplete = DriverCompleteTemplate.bind({});
 DriverComplete.args = {
-  ...flatten({
-    drawConfiguration: {
-      ...defaultArgs.drawConfiguration,
-      axesSpacing: 150,
-      axisHeight: 300,
-      margin: { ...defaultArgs.drawConfiguration.margin, left: 50, right: 50 },
-      coloring: {
-        ...defaultArgs.drawConfiguration.coloring,
-        coloringAxis: "Alc. Impaired",
+  ...flatten(
+    {
+      drawConfiguration: {
+        ...defaultArgs.drawConfiguration,
+        axesSpacing: 150,
+        axisHeight: 300,
+        margin: {
+          ...defaultArgs.drawConfiguration.margin,
+          left: 50,
+          right: 50,
+        },
+        coloring: {
+          ...defaultArgs.drawConfiguration.coloring,
+          coloringAxis: "Alc. Impaired",
+        },
       },
     },
-  }, { delimiter }),
+    { delimiter }
+  ),
   data: badDriverComplete,
 };
 
 export const IrisComplete = IrisCompleteTemplate.bind({});
 IrisComplete.args = {
-  ...flatten({
-    drawConfiguration: {
-      ...defaultArgs.drawConfiguration,
-      axesSpacing: 150,
-      axisHeight: 200,
-      margin: { ...defaultArgs.drawConfiguration.margin, left: 50, right: 50 },
+  ...flatten(
+    {
+      drawConfiguration: {
+        ...defaultArgs.drawConfiguration,
+        axesSpacing: 150,
+        axisHeight: 200,
+        margin: {
+          ...defaultArgs.drawConfiguration.margin,
+          left: 50,
+          right: 50,
+        },
+      },
     },
-  }, { delimiter }),
+    { delimiter }
+  ),
   data: irisComplete,
 };
 
 export const MpgComplete = MpgCompleteTemplate.bind({});
 MpgComplete.args = {
-  ...flatten({
-    drawConfiguration: {
-      ...defaultArgs.drawConfiguration,
-      axesSpacing: 150,
-      axisHeight: 300,
-      coloring: {
-        ...defaultArgs.drawConfiguration.coloring,
-        coloringAxis: "origin",
+  ...flatten(
+    {
+      drawConfiguration: {
+        ...defaultArgs.drawConfiguration,
+        axesSpacing: 150,
+        axisHeight: 300,
+        coloring: {
+          ...defaultArgs.drawConfiguration.coloring,
+          coloringAxis: "origin",
+        },
       },
     },
-  }, { delimiter }),
+    { delimiter }
+  ),
   data: mpgComplete,
 };
 
 export const PenguinComplete = PenguinCompleteTemplate.bind({});
 PenguinComplete.args = {
-  ...flatten({
-    drawConfiguration: {
-      ...defaultArgs.drawConfiguration,
-      axesSpacing: 150,
-      axisHeight: 350,
-      margin: { ...defaultArgs.drawConfiguration.margin, left: 30, right: 30 },
+  ...flatten(
+    {
+      drawConfiguration: {
+        ...defaultArgs.drawConfiguration,
+        axesSpacing: 150,
+        axisHeight: 350,
+        margin: {
+          ...defaultArgs.drawConfiguration.margin,
+          left: 30,
+          right: 30,
+        },
+      },
     },
-  }, { delimiter }),
+    { delimiter }
+  ),
   data: penguinsComplete,
 };
